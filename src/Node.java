178c606671f83709aa81ff94e2214fd3d366fa8d
jourys <146828726+jourys@users.noreply.github.com>
2023-10-07 23:54:18 +0300

    Update Node.java

--- src/Node.java
+++ src/Node.java
@@ -1,22 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author batoolalfouzan
- */
 
 package project;
 
 
-public class node<t> {
-public Contact data;
-public node <Contact> next;
-public node<Contact> prev;
 
-    public node(Contact d) {
+public class node<t> {
+public t data;
+public node <t> next;
+public node<t> prev;
+
+    public node(t d) {
         this.data = d;
         next=null;
         prev=null;
@@ -24,4 +16,7 @@ public node<Contact> prev;
         
     }
 
+    public t getData() {
+        return data;
+    }
 }
