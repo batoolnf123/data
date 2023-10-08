9a657654d1ea22a22e252d903e1178a6c5b2daf2
anoudmu <146832005+anoudmu@users.noreply.github.com>
2023-10-09 02:13:00 +0300

    Update linkedlist.java

--- src/javaapplication1/linkedlist.java
+++ src/javaapplication1/linkedlist.java
@@ -5,138 +5,138 @@ import javaapplication1.node;
 
 
 
-public class linkedlist<T> 
-{
- private node<T> head;
- private node<T> current;
+public class linkedlist<T> {
 
+    private node<T> head;
+    private node<T> current;
 
     public linkedlist(node<T> h) //joury
- {
+    {
         head = h;
-        
+
     }
 
+    public void add(T newContact) //joury
+    {
+        if (head == null) {
+            head = new node<T>(newContact);
+        } else {
+            current.next = new node<T>(newContact);
+        }
 
-public void add(T newContact) //joury
-{
-        if (head == null) 
-            head = new node<T> (newContact);
-       
-        else
-          current.next=new node<T>(newContact);
-        
         current = current.next;
         System.out.println("Contact added successfully!");
-}
+    }
 
-public Contact searchbyName(String name) //joury
-{
- if (head==null)
-     return null;
- else
-     current=head;
- 
- while(current!=null)
-     {
-         if (((Contact)current.getData()).getContactName().equals(name))
-             return (Contact)current.getData();
-         else 
-           current=current.next;
-     }
- return null; 
-}
+    public Contact searchbyName(String name) //joury
+    {
+        if (head == null) {
+            return null;
+        } else {
+            current = head;
+        }
 
-public Contact searchbyemail(String email) //joury
-{
- if (head==null)
-     return null;
- else
-     current=head;
+        while (current != null) {
+            if (((Contact) current.getData()).getContactName().equals(name)) {
+                return (Contact) current.getData();
+            } else {
+                current = current.next;
+            }
+        }
+        return null;
+    }
 
- while(current!=null)
-     {
-         if (((Contact)current.getData()).getEmailAddress().equals(email))
-             return (Contact)current.getData();
+    public Contact searchbyemail(String email) //joury
+    {
+        if (head == null) {
+            return null;
+        } else {
+            current = head;
+        }
 
-         else 
-             current=current.next;
+        while (current != null) {
+            if (((Contact) current.getData()).getEmailAddress().equals(email)) {
+                return (Contact) current.getData();
+            } else {
+                current = current.next;
+            }
 
-             }
- return null; 
-}
-public Contact searchbyphonenum(String num) //joury
-{
- if (head==null)
-     return null;
- else
-     current=head;
- 
- while(current!=null)
-     {
-         if (((Contact)current.getData()).getPhoneNumber().equals(num))
-             
-             return (Contact)current.getData();
-         else 
-           current=current.next;
-     }
- 
-  return null; 
-}
+        }
+        return null;
+    }
 
-public Contact searchbyaddress(String add) //joury
-{
- if (head==null)
-     return null;
- else
-     current=head;
- 
- while(current!=null)
-     {
-         if (((Contact)current.getData()).getAddress().equals(add))
-             
-             return (Contact)current.getData();
-         
-         else 
-           current=current.next;
-     }
- 
-  return null; 
-}
+    public Contact searchbyphonenum(String num) //joury
+    {
+        if (head == null) {
+            return null;
+        } else {
+            current = head;
+        }
 
-public Contact searchbybirthday(String birth) //joury
-{
- if (head==null)
-     return null;
- else
-     current=head;
- 
- while(current!=null)
-     {
-         if (((Contact)current.getData()).getBirthday().equals(birth))
-             
-           return (Contact)current.getData();
+        while (current != null) {
+            if (((Contact) current.getData()).getPhoneNumber().equals(num)) {
+                return (Contact) current.getData();
+            } else {
+                current = current.next;
+            }
+        }
 
-         else 
-           current=current.next;
-     }
-    return null; 
-}
+        return null;
+    }
 
-public void deletContact(node<Contact> delet) //joury
-{
- if (head==null)
-     return;
- else current=head;
- while(current!=null)
- {
-     if(current.data==delet.data)
-         current.prev.next=current.next;
-     
-     current=current.next;
- }
-     
-}
+    public Contact searchbyaddress(String add) //joury
+    {
+        if (head == null) {
+            return null;
+        } else {
+            current = head;
+        }
+
+        while (current != null) {
+            if (((Contact) current.getData()).getAddress().equals(add)) {
+                return (Contact) current.getData();
+            } else {
+                current = current.next;
+            }
+        }
+
+        return null;
+    }
+
+    public Contact searchbybirthday(String birth) //joury
+    {
+        if (head == null) {
+            return null;
+        } else {
+            current = head;
+        }
+
+        while (current != null) {
+            if (((Contact) current.getData()).getBirthday().equals(birth)) {
+                return (Contact) current.getData();
+            } else {
+                current = current.next;
+            }
+        }
+        return null;
+    }
+
+    public void deletContact(node<Contact> delet) //joury
+    {
+        if (head == null) {
+            return;
+        } else {
+            current = head;
+        }
+        while (current != null) {
+            if (current.data == delet.data) {
+                current.prev.next = current.next;
+            }
+
+            current = current.next;
+        }
+
+    }
 
     public void printList() //joury
     {
@@ -147,75 +147,84 @@ public void deletContact(node<Contact> delet) //joury
         }
         System.out.println();
     }
-   
+
     public boolean isEmpty() //joury
     {
-        return head==null;
+        return head == null;
     }
-    
-    public boolean last () //joury
-    {
-return current.next == null;
-    } 
-    public void findfirst () //joury
-    {
-current = head;
-    }
-public void findnext () //joury
-{
-current = current.next; 
-}
-public T retrieve() //joury
- {
-return current.data; 
- }
-public void update (T x) //joury
-{
-current.data = x;
-}
-public boolean full () //joury
-{
-return false;
-}
-public void remove () //joury
- {
-if (current == head)
-{
-head = head.next;
-}
-else {
-node<T> extranode = head;
 
-while (extranode.next != current)
-    extranode = extranode.next;
-
-extranode.next = current.next;
-     }
-if (current.next == null)
-    current = head;
-else
-    current = current.next;
-}
-public void insert (T d) 
- { 
-    node<T> p=new node<T>(d);
-    if(head==null){
-        head=p;
-        current=p;
+    public boolean last() //joury
+    {
+        return current.next == null;
     }
-    else{
-        p.next=current.next;
-        current.next=p;
-        current=p;
-    }}
-    public boolean search(T x){
-        node<T> temp=head;
-        while(temp!=null){
-            if(temp.data.equals(x))
+
+    public void findfirst() //joury
+    {
+        current = head;
+    }
+
+    public void findnext() //joury
+    {
+        current = current.next;
+    }
+
+    public T retrieve() //joury
+    {
+        return current.data;
+    }
+
+    public void update(T x) //joury
+    {
+        current.data = x;
+    }
+
+    public boolean full() //joury
+    {
+        return false;
+    }
+
+    public void remove() //joury
+    {
+        if (current == head) {
+            head = head.next;
+        } else {
+            node<T> extranode = head;
+
+            while (extranode.next != current) {
+                extranode = extranode.next;
+            }
+
+            extranode.next = current.next;
+        }
+        if (current.next == null) {
+            current = head;
+        } else {
+            current = current.next;
+        }
+    }
+
+    public void insert(T d) {
+        node<T> p = new node<T>(d);
+        if (head == null) {
+            head = p;
+            current = p;
+        } else {
+            p.next = current.next;
+            current.next = p;
+            current = p;
+        }
+    }
+
+    public boolean search(T x) {
+        node<T> temp = head;
+        while (temp != null) {
+            if (temp.data.equals(x)) {
                 return true;
-            else
-                temp=temp.next;
-        } return false;
+            } else {
+                temp = temp.next;
+            }
+        }
+        return false;
     }
 //    batool wrote insert and search, Joury wrote the rest
 
