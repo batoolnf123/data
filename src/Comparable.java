dbbbc84ab4133c6c63517a6662793a832ac7dd7f
jourys <146828726+jourys@users.noreply.github.com>
2023-10-08 14:18:28 +0300

    Update Comparable.java

--- src/Comparable.java
+++ src/Comparable.java
@@ -1,18 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
-
-/**
- *
- * @author batoolalfouzan
- */
-
 package project;
 
-interface Comparable {
+interface Comparable<T> {
     
-  public void CompareTo(String s1, String s2);  
+  public int CompareTo(String second);  
   public void display();
 }
 
