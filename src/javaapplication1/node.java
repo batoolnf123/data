be9e0e42c5620c347e50c5ac5cde8fb00b1a11b1
jourys <146828726+jourys@users.noreply.github.com>
2023-10-08 21:48:55 +0300

    Update node.java

--- src/javaapplication1/node.java
+++ src/javaapplication1/node.java
@@ -1,24 +1,18 @@
 package javaapplication1;
 
 
-  /* Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
-  */
-
- /**
-  *
-  * @author batoolalfouzan
-  */
 
 
 
 
-public class node<T> {
+public class node<T> //joury
+  {
 public T data;
 public node <T> next;
 public node<T> prev;
 
-    public node(T d) {
+    public node(T d) //joury
+    {
         this.data = d;
         next=null;
         prev=null;
@@ -26,7 +20,8 @@ public node<T> prev;
         
     }
 
-    public T getData() {
+    public T getData() //joury
+    {
         return data;
     }
     
