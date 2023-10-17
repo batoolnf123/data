efda285f18ec8e4f28e020e7a8fa7a6bd12847db
anoudmu <146832005+anoudmu@users.noreply.github.com>
2023-10-17 18:38:59 +0300

    Update linkedlist.java

--- src/javaapplication1/linkedlist.java
+++ src/javaapplication1/linkedlist.java
@@ -13,11 +13,11 @@ public class linkedlist<T> {
 
     public linkedlist(node<T> h) //joury
     {
-        head =current = h;
+        head = current = h;
 
     }
 
-    public void deletContact(node<Contact> delet) //joury
+    /* public void deletContact(node<Contact> delet) //joury
     {
         if (head == null) {
             return;
@@ -31,19 +31,17 @@ public class linkedlist<T> {
 
             current = current.next;
         }
-    }
-
+    }*/
     public Contact searchByName(String name) //joury
     {
         if (head == null) {
             return null;
-        } 
-         
-            current = head;
-    
+        }
+
+        current = head;
+
         while (current != null) {
-            
-            
+
             if (((Contact) current.getData()).getContactName().equals(name)) {
                 return (Contact) current.getData();
             }
@@ -53,9 +51,9 @@ public class linkedlist<T> {
         return null;
     }
 
-    public linkedlist<Contact> SearchByEmail(String email) //joury //0
+    public linkedlist<Contact> SearchByEmail(String email) //joury //
     {
-                        linkedlist<Contact> list=new linkedlist<Contact>();
+        linkedlist<Contact> list = new linkedlist<Contact>();
 
         if (head == null) { //1
             return null; //1
@@ -65,18 +63,17 @@ public class linkedlist<T> {
 
         while (current != null) {//n+1
             if (((Contact) current.getData()).getEmailAddress().equals(email)) {//{n(1)
-                 list.insert((  Contact)current.getData());
-            } 
-                current = current.next;// n*1
-    }
+                list.insert((Contact) current.getData());
+            }
+            current = current.next;// n*1
+        }
 
-        
         return list;
     }
 
     public linkedlist<Contact> SearchByPhoneNumber(String num) //joury 
     {
-      linkedlist<Contact> list=new linkedlist<Contact>();
+        linkedlist<Contact> list = new linkedlist<Contact>();
 
         if (head == null) {
             return null;
@@ -86,10 +83,10 @@ public class linkedlist<T> {
 
         while (current != null) {
             if (((Contact) current.getData()).getPhoneNumber().equals(num)) {
-                 list.insert((  Contact)current.getData());
+                list.insert((Contact) current.getData());
             }
-                current = current.next;
-            
+            current = current.next;
+
         }
 
         return list;
@@ -97,7 +94,7 @@ public class linkedlist<T> {
 
     public linkedlist<Contact> SearchByAddress(String add) //joury
     {
-                linkedlist<Contact> list=new linkedlist<Contact>();
+        linkedlist<Contact> list = new linkedlist<Contact>();
 
         if (head == null) {
             return null;
@@ -107,10 +104,10 @@ public class linkedlist<T> {
 
         while (current != null) {
             if (((Contact) current.getData()).getAddress().equals(add)) {
-                 list.insert((  Contact)current.getData());
-            } 
-                current = current.next;
-            
+                list.insert((Contact) current.getData());
+            }
+            current = current.next;
+
         }
 
         return list;
@@ -118,21 +115,24 @@ public class linkedlist<T> {
 
     public linkedlist<Contact> SearchByBirthday(String birth) //joury
     {
-        linkedlist<Contact> list=new linkedlist<Contact>();
+        linkedlist<Contact> list = new linkedlist<Contact>();
         if (head == null) {
             return null;
         } else {
             current = head;
-        
 
-        while (current != null) {
-            if (((Contact) current.getData()).getBirthday().equals(birth)) 
-                 list.insert((  Contact)current.getData());
+            while (current != null) {
+                if (((Contact) current.getData()).getBirthday().equals(birth)) {
+                    list.insert((Contact) current.getData());
+                }
+
                 current = current.next;
+            }
+
+            return list;
         }
-      
-        return list;
-    }}
+    }
+
     public void printList() //joury
     {
         current = head;
@@ -222,60 +222,80 @@ public class linkedlist<T> {
         return false;
     }*/
 //    batool wrote insert and search, Joury wrote the rest
+    /*public void addSortedPOEA(T data) {//another one check tab
+        Node<T> newNode = new Node<>(data);
+
+        if (head == null || data.compareTo(head.data) < 0) {
+            newNode.next = head;
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            while (current.next != null && data.compareTo(current.next.data) >= 0) { //to move until finding 
+                current = current.next;
+            }
+
+            newNode.next = current.next;
+            current.next = newNode;
+        }
+    }*/
 
     
- 
-
-
-
-
+    
     public void addSortedContact(T d) //joury 
     {
-        node<T> p = new node<T>(d);
-        //case1
+        node<T> newContact = new node<T>(d);
+        //case1 if empty
         if (head == null) {
-            head = p;
-            current = p;
-        } //case2
-        else {//if smaller than first
+            head = newContact;
+            current = newContact;
+        } //case2 if smaller than first
+        else {
             if (((Contact) d).CompareTo((Contact) head.data) < 0) {
-                p.next = head;
-                head = p;
+                newContact.next = head;
+                head = newContact;
                 return;
+
             } else {
-                node<T> r = head, q = null;
-                while (r != null && (((Contact) r.data).CompareTo((Contact) d) <= 0)) {
-                    q = r;
-                    r = r.next;
+                node<T> temp = head;
+                while (temp.next != null && (((Contact) d).CompareTo((Contact) temp.data) >= 0)) {
+                    temp = temp.next;
                 }
-                q.next = p;
-                p.next = r;
+                newContact.next = temp.next;
+                temp.next = newContact;
+
             }
         }
     }
 
-    public void addSortedEvent(T d) //anoud
+    public void addSortedEvent(T e) //anoud
     {
-        node<T> p = new node<T>(d);
-        //case1
+        node<T> newEvent = new node<T>(e);
+        //case1 if empty
         if (head == null) {
-            head = current = p;
-        } //case2
-        else {//if smaller than first
-            if (((Event) d).CompareTo((Event) head.data) < 0) {
-                p.next = head;
-                head = p;
+            head = newEvent;
+            current = newEvent;
+        
+        } //case2 if smaller than first
+        else {
+            if (((Event) e).CompareTo((Event) head.data) < 0) {
+                newEvent.next = head;
+                head = newEvent;
                 return;
+
             } else {
-                node<T> r = head, q = null;
-                while (r != null && (((Event) r.data).CompareTo((Event) d) <= 0)) {
-                    q = r;
-                    r = r.next;
+                node<T> temp = head;
+                while (temp.next != null && (((Event) e).CompareTo((Event) temp.data) >= 0)) {// search for position of the new node 
+                    temp = temp.next;
                 }
-                q.next = p;
-                p.next = r;
+                newEvent.next = temp.next;
+                temp.next = newEvent;
+
             }
         }
-
     }
+    
+    
+    
+    
+
 }
